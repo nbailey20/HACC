a755de72492c14bfb0c157133a028f59b1a118c1
Nick <nickdbailey20@gmail.com>
2021-12-10 19:28:32 -0600

    significant code cleanup into functions

--- hacc_install.py
+++ hacc_install.py
@@ -1,84 +1,119 @@
 import hacc_vars
 import boto3, subprocess, json
 
-def install(args):
-    account = boto3.client('sts').get_caller_identity().get('Account')
-    kms = boto3.client('kms', region_name=hacc_vars.aws_hacc_region)
-    iam = boto3.client('iam')
+VAULT_IAM_PERMS = """
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ssm:DescribeParameters",
+                "ssm:GetParameter*",
+                "ssm:DeleteParameter*",
+                "ssm:PutParameter"
+            ],
+            "Resource": "%s"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "kms:Encrypt",
+                "kms:Decrypt"
+            ],
+            "Resource": "%s"
+        }
+    ]
+}
+"""
 
-    hacc_kms = kms.create_key(
-        KeyUsage='ENCRYPT_DECRYPT',
-        KeySpec='SYMMETRIC_DEFAULT'
-    )
-    if args.debug: print('Created new KMS CMK: ', hacc_kms['ResponseMetadata']['HTTPStatusCode'])
-
-    hacc_alias = kms.create_alias(
-        AliasName='alias/{key}'.format(key=hacc_vars.aws_hacc_kms_alias),
-        TargetKeyId=hacc_kms['KeyMetadata']['Arn']
-    )
-    if args.debug: print('Created new KMS alias: ', hacc_alias['ResponseMetadata']['HTTPStatusCode'])
-
-    hacc_user = iam.create_user(
-        UserName=hacc_vars.aws_hacc_uname
-    )
-    if args.debug: print('Created new AWS user: ', hacc_user['ResponseMetadata']['HTTPStatusCode'])
-
-    hacc_creds = iam.create_access_key(
-        UserName=hacc_vars.aws_hacc_uname
-    )
-    if args.debug: print('Created new AWS creds for user: ', hacc_creds['ResponseMetadata']['HTTPStatusCode'])
-
+def create_hacc_profile(access_key_id, secret_access_key, debug):
+    # Adds a new profile to AWS credentials/config file
     aws_config_region = subprocess.run(['aws', 'configure', 'set', 
         'region', hacc_vars.aws_hacc_region, 
         '--profile', hacc_vars.aws_hacc_uname]
     )
-    if args.debug: print('Set vault AWS region: ', aws_config_region.returncode)
+    if debug: print('INFO: Successfully wrote vault AWS region to profile')
 
     aws_config_access = subprocess.run(['aws', 'configure', 'set', 
-        'aws_access_key_id', hacc_creds['AccessKey']['AccessKeyId'], 
+        'aws_access_key_id', access_key_id, 
         '--profile', hacc_vars.aws_hacc_uname]
     )
-    if args.debug: print('Set AWS user access key: ', aws_config_access.returncode)
+    if debug: print('INFO: Successfully wrote vault AWS access key to profile')
 
     aws_config_secret = subprocess.run(['aws', 'configure', 'set', 
-        'aws_secret_access_key', hacc_creds['AccessKey']['SecretAccessKey'], 
+        'aws_secret_access_key', secret_access_key, 
         '--profile', hacc_vars.aws_hacc_uname]
     )
-    if args.debug: print('Set AWS user secret key: ', aws_config_secret.returncode)
+    if debug: print('INFO: Successfully wrote vault AWS secret key to profile')
+    return
 
-    vault_perms = {
-        'Version': '2012-10-17',
-        'Statement': [
-            {
-                'Effect': 'Allow',
-                'Action': [
-                    'ssm:DescribeParameters',
-                    'ssm:GetParameter*',
-                    'ssm:DeleteParameter*',
-                    'ssm:PutParameter'
-                ],
-                'Resource': 'arn:aws:ssm:{region}:{account}:parameter/{path}/*'.format(
-                                region=hacc_vars.aws_hacc_region, 
-                                account=account,
-                                path=hacc_vars.aws_hacc_param_path
-                            )
-            },
-            {
-                'Effect': 'Allow',
-                'Action': [
-                    'kms:Encrypt',
-                    'kms:Decrypt'
-                ],
-                'Resource': hacc_kms['KeyMetadata']['Arn']
-            }
-        ]
-    }
-    hacc_policy = iam.put_user_policy(
+
+def aws_call(client, api, debug, **kwargs):
+    # Execute generic AWS API call with error handling
+    try:
+        method_to_call = getattr(client, api)
+        if debug: print('INFO: About to call API {}'.format(api))
+    except:
+        if debug: print('ERROR: API {} not known'.format(api))
+
+    try:
+        result = method_to_call(**kwargs)
+        if debug: print('INFO: API execution successful')
+        return result
+    except Exception as e:
+        if debug: print('ERROR: API call failed, exiting.: {}'.format(e))
+        exit(1)
+    
+
+def install(args):
+    # Setup IAM user and KMS CMK for Vault
+    account = boto3.client('sts').get_caller_identity().get('Account')
+    kms = boto3.client('kms', region_name=hacc_vars.aws_hacc_region)
+    iam = boto3.client('iam')
+    debug = args.debug
+
+    hacc_kms = aws_call(
+                kms, 'create_key', debug, 
+                KeyUsage='ENCRYPT_DECRYPT', 
+                KeySpec='SYMMETRIC_DEFAULT'
+                )
+
+    aws_call(
+        kms, 'create_alias', debug, 
+        AliasName='alias/{key}'.format(key=hacc_vars.aws_hacc_kms_alias), 
+        TargetKeyId=hacc_kms['KeyMetadata']['Arn']
+    )
+
+    aws_call(
+        iam, 'create_user', debug, 
+        UserName=hacc_vars.aws_hacc_uname
+    )
+
+    hacc_creds = aws_call(
+                    iam, 'create_access_key', debug, 
+                    UserName=hacc_vars.aws_hacc_uname
+                )
+
+    create_hacc_profile(
+        hacc_creds['AccessKey']['AccessKeyId'], 
+        hacc_creds['AccessKey']['SecretAccessKey'], 
+        debug
+    )
+
+    vault_path_arn = 'arn:aws:ssm:{region}:{account}:parameter/{path}/*'.format(
+                        region=hacc_vars.aws_hacc_region, 
+                        account=account,
+                        path=hacc_vars.aws_hacc_param_path
+                    )
+    vault_key_arn = hacc_kms['KeyMetadata']['Arn']
+    perms = json.loads(VAULT_IAM_PERMS % (vault_path_arn, vault_key_arn))
+    aws_call(
+        iam, 'put_user_policy', debug, 
         UserName=hacc_vars.aws_hacc_uname,
         PolicyName=hacc_vars.aws_hacc_policy,
-        PolicyDocument=json.dumps(vault_perms)
+        PolicyDocument=json.dumps(perms)
     )
-    if args.debug: print('Created IAM policy for user: ', hacc_policy['ResponseMetadata']['HTTPStatusCode'])
 
     print('Vault setup complete.')
     return
--- hacc_uninstall.py
+++ hacc_uninstall.py
@@ -1,46 +1,48 @@
 import hacc_vars
 from hacc_add_remove_param import get_kms_id
+from hacc_install import aws_call
 import boto3, subprocess
 
 def eradicate(args):
+    print('Eradicating Vault...')
     iam = boto3.client('iam')
     kms = boto3.client('kms', region_name=hacc_vars.aws_hacc_region)
+    debug = args.debug
 
     hacc_key_id = get_kms_id(args.debug, kms)
-    kms_delete = kms.schedule_key_deletion(
+    aws_call(
+        kms, 'schedule_key_deletion', debug, 
         KeyId=hacc_key_id,
         PendingWindowInDays=7
     )
-    if args.debug: print('Deleting AWS KMS CMK: ', kms_delete['ResponseMetadata']['HTTPStatusCode'])
 
-    alias_delete = kms.delete_alias(
+    aws_call(
+        kms, 'delete_alias', debug, 
         AliasName='alias/{key}'.format(key=hacc_vars.aws_hacc_kms_alias)
     )
-    if args.debug: print('Deleting AWS KMS alias: ', alias_delete['ResponseMetadata']['HTTPStatusCode'])
 
-    policy_delete = iam.delete_user_policy(
+    aws_call(
+        iam, 'delete_user_policy', debug, 
         UserName=hacc_vars.aws_hacc_uname,
         PolicyName=hacc_vars.aws_hacc_policy
     )
-    if args.debug: print('Deleting AWS IAM policy: ', policy_delete['ResponseMetadata']['HTTPStatusCode'])
 
     aws_access_key = subprocess.run(['aws', 'configure', 'get', 
         'aws_access_key_id', '--profile', hacc_vars.aws_hacc_uname],
         capture_output=True, text=True
     )
     aws_access_key = aws_access_key.stdout.strip()
-    if args.debug: print('Retrieved saved AWS user access key: ', aws_access_key)
-
-    creds_delete = iam.delete_access_key(
+    if args.debug: print('INFO: Retrieved saved AWS user access key')
+    aws_call(
+        iam, 'delete_access_key', debug, 
         UserName=hacc_vars.aws_hacc_uname,
         AccessKeyId=aws_access_key
     )
-    if args.debug: print('Removing access key from AWS user: ', creds_delete['ResponseMetadata']['HTTPStatusCode'])
 
-    user_delete = iam.delete_user(
-        UserName=hacc_vars.aws_hacc_uname
+    aws_call(
+        iam, 'delete_user', debug, 
+        UserName=hacc_vars.aws_hacc_uname,
     )
-    if args.debug: print('Deleting AWS user: ', user_delete['ResponseMetadata']['HTTPStatusCode'])
 
     ## Remove profile from ~/.aws/config and credentials! read/regex/write Python
     print('Vault has been eradicated.')
