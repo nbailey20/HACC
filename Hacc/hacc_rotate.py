2981e2c739f4a927e16956cf624525d9b07ef376
Nick <nickdbailey20@gmail.com>
2023-05-02 18:11:46 -0500

    rotate working with console

--- Hacc/hacc_rotate.py
+++ Hacc/hacc_rotate.py
@@ -1,15 +1,13 @@
 import time
 
+from console.hacc_console import console
 from hacc_add import add
 from hacc_delete import delete
 
 
-def rotate(display, args, config):
-    delete(display, args, config)
-    display.update(
-        display_type='text_append',
-        data={'text': 'Sleeping for 3 seconds to fully delete before updating...'}
-    )
+def rotate(args, config):
+    delete(args, config)
+    console.print('Sleeping for 3 seconds to fully delete before updating...')
     time.sleep(3)
-    add(display, args, config)
+    add(args, config)
     return
